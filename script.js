c649f42cedd34ee85ae176d90953faeacbcdfc56
Sudip Ruidas <sudipkgec23@gmail.com>
2025-02-16 00:47:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,10 @@
 
 
 
-document.querySelector(".btn-left button").addEventListener("click", function(){
-    let otm= document.querySelector(".open-text");
-    otm.style.opacity = 1;
+document.querySelector("button").addEventListener("click", function(){
+    let text = document.querySelector(".open-text");
+    text.style.opacity = 1;
    
 })
 
-document.querySelector(".btn-right button").addEventListener("click", function(){
-    let gift= document.querySelector(".gift-text");
-    gift.style.opacity = 1;
-   
-})
+
