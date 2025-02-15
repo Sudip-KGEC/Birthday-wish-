f1efc2fef375f493381b67b2ea8f994a52187964
Sudip Ruidas <sudipkgec23@gmail.com>
2025-02-16 00:15:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+document.querySelector(".btn-left button").addEventListener("click", function(){
+    let otm= document.querySelector(".open-text");
+    otm.style.opacity = 1;
+   
+})
+
+document.querySelector(".btn-right button").addEventListener("click", function(){
+    let gift= document.querySelector(".gift-text");
+    gift.style.opacity = 1;
+   
+})
